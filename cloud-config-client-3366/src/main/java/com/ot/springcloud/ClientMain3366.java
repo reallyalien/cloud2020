e612969b32b57e72eb6b38bbbb9cbeeaba096d52
hello world <reallyalien@yeah.net>
2020-06-14 22:26:09 +0800

    spring cloud config and bus

---
+++ cloud-config-client-3366/src/main/java/com/ot/springcloud/ClientMain3366.java
@@ -0,0 +1,14 @@
+package com.ot.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ClientMain3366 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientMain3366.class,args);
+    }
+}
